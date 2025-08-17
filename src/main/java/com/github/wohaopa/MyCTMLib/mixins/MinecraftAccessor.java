ddf02402e3a5dbe0c7e37b8a7a10242cf47fbd7e
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-08-17 14:00:24 +0800

    1.1.5

---
+++ src/main/java/com/github/wohaopa/MyCTMLib/mixins/MinecraftAccessor.java
@@ -0,0 +1,14 @@
+package com.github.wohaopa.MyCTMLib.mixins;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.data.IMetadataSerializer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(value = Minecraft.class)
+public interface MinecraftAccessor {
+
+    @Accessor("metadataSerializer_")
+    IMetadataSerializer getMetadataSerializer();
+}
