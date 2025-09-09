b57c49c8272cb136415fd24bb9d6f293b7a939e4
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-09-10 01:48:51 +0800

    bugfix

--- src/main/java/com/github/wohaopa/MyCTMLib/GTNHIntegrationHelper.java
+++ src/main/java/com/github/wohaopa/MyCTMLib/GTNHIntegrationHelper.java
@@ -1,5 +1,7 @@
 package com.github.wohaopa.MyCTMLib;
 
+import java.lang.reflect.Field;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockAir;
 import net.minecraft.client.renderer.Tessellator;
@@ -10,12 +12,31 @@ import net.minecraftforge.common.util.ForgeDirection;
 
 import com.gtnewhorizon.gtnhlib.client.renderer.TessellatorManager;
 
+import bartworks.common.blocks.BWBlocksGlass;
+import bartworks.common.blocks.BWBlocksGlass2;
+import bartworks.common.loaders.ItemRegistry;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.tileentity.ITexturedTileEntity;
+import gregtech.common.blocks.BlockCasings4;
 import gregtech.common.blocks.BlockMachines;
 import gregtech.common.render.GTCopiedBlockTextureRender;
+import gtPlusPlus.xmod.gregtech.common.blocks.GregtechMetaCasingBlocks3;
 
 public class GTNHIntegrationHelper {
+
+    public static Field connectedTexBWGlassField;
+    public static Field connectedTexBWGlass2Field;
+
+    static {
+        try {
+            connectedTexBWGlassField = BWBlocksGlass.class.getDeclaredField("connectedTex");
+            connectedTexBWGlassField.setAccessible(true);
+            connectedTexBWGlass2Field = BWBlocksGlass2.class.getDeclaredField("connectedTex");
+            connectedTexBWGlass2Field.setAccessible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Tessellator getGTNHLibTessellator() {
         return TessellatorManager.get();
@@ -39,5 +60,33 @@ public class GTNHIntegrationHelper {
             return block.getIcon(blockAccess, x, y, z, forgeDirection.ordinal());
         }
         return null;
+    }
+
+    public static void setBlockCasings4CTM(boolean ctm) {
+        BlockCasings4.mConnectedMachineTextures = ctm;
+    }
+
+    public static void setGregtechMetaCasingBlocks3CTM(boolean ctm) {
+        GregtechMetaCasingBlocks3.mConnectedMachineTextures = ctm;
+    }
+
+    public static boolean isInit = false;
+
+    public static void setBWBlocksGlassCTM(boolean ctm) {
+        try {
+            if (connectedTexBWGlassField == null) return;
+            if (!isInit) {
+                isInit = true;
+                return;
+            }
+            if (ItemRegistry.bw_realglas != null) {
+                connectedTexBWGlassField.set(ItemRegistry.bw_realglas, ctm);
+            }
+            if (ItemRegistry.bw_realglas2 != null) {
+                connectedTexBWGlass2Field.set(ItemRegistry.bw_realglas2, ctm);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/github/wohaopa/MyCTMLib/mixins/MixinSimpleReloadableResourceManager.java
+++ src/main/java/com/github/wohaopa/MyCTMLib/mixins/MixinSimpleReloadableResourceManager.java
@@ -9,11 +9,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.github.wohaopa.MyCTMLib.GTNHIntegrationHelper;
+
+import cpw.mods.fml.common.Loader;
+
 @Mixin(value = SimpleReloadableResourceManager.class)
 public class MixinSimpleReloadableResourceManager {
 
     @Inject(method = "clearResources", at = @At("HEAD"))
     private void onClearResources(CallbackInfo ci) {
         ctmIconMap.clear();
+        if (Loader.isModLoaded("gregtech")) {
+            GTNHIntegrationHelper.setBlockCasings4CTM(true);
+            GTNHIntegrationHelper.setGregtechMetaCasingBlocks3CTM(true);
+            GTNHIntegrationHelper.setBWBlocksGlassCTM(true);
+        }
     }
 }
