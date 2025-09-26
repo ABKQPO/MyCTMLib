920d226c0289b235bb2f3e564c95933f1a981f47
称明 <8456918+Windorain@users.noreply.github.com>
2025-09-27 00:25:03 +0800

    修正注释

--- src/main/java/com/github/wohaopa/MyCTMLib/mixins/MixinTextureMap.java
+++ src/main/java/com/github/wohaopa/MyCTMLib/mixins/MixinTextureMap.java
@@ -99,7 +99,7 @@ public abstract class MixinTextureMap extends AbstractTexture implements ITickab
                             .replace("textures/blocks/", "")
                             .replace(".png", "");
 
-                        // 疑似测试代码
+                        // 修复代码
                         if (connectTextureName.startsWith("gregtech:iconsets/MACHINE_CASING_FUSION_")
                             && connectTexture.endsWith("_ctm")
                             && Loader.isModLoaded("gregtech")) {
